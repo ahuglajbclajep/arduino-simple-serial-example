bdf5eab33e91452f77163a6147fa4aec26f15ea6
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-02-14 10:34:21 +0900

    [upd]launch option

--- README.md
+++ README.md
@@ -15,9 +15,16 @@ python3 main.py
 ```  
 and Arduino setup.
 
+###Run
+```
+python3 main.py -p COM3 # ポートの指定
+python3 main.py -r 9600 # 転送レートの指定
+python3 main.py -s # 使用するポートを表示
+```
+
 ## Future Releases
 * オプションで指定したものを起動するとか？
-* なにか案があれば[issues](https://github.com/ahuglajbclajep/kuso-arduino-button/issues)にでもどうぞ
+* なにか案があれば[issues](https://github.com/ahuglajbclajep/kuso-arduino-button/issues)にどうぞ
 
 ## Contribution
 1. Fork it  
--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import sys
 import argparse
 import serial
+import serial.tools.list_ports
 
 
 def main():
-    parser = argparse.ArgumentParser(description='COM3ポートでArduinoとシリアル通信をします')
+    parser = argparse.ArgumentParser(description="Arduinoとシリアル通信をします")
+    parser.add_argument("-p", "--port", help="ポートの指定")
+    parser.add_argument("-r", "--rate", type=int, default=9600, help="レートの指定")
+    parser.add_argument("-s", "--show", action="store_true", help="使用するポートを表示")
     args = parser.parse_args()
-    run()
+
+    run(args.port, args.rate, args.show)
 
 
-def run():
-    port = 'COM3'  # OS依存, serial.tools.list_ports.comports() で調べられる
+def run(port, rate, show):
     try:
-        ser = serial.Serial(port, 9600)
+        if port is None:
+            port = serial.tools.list_ports.comports()[0][0]  # 最初のポートのdeviceを取得
+
+        if show:
+            print("device:" + port)
+
+        ser = serial.Serial(port, rate)
+    except IndexError:  # ポートの自動取得に失敗したとき
+        sys.stderr.write("ポートが検出できませんでした。")
+        sys.exit(1)
     except:
         sys.exit(sys.exc_info()[1])
 
     while True:
-        print(ser.read(1).decode('utf-8'))  # 1バイト読んで文字列に変換
+        if ser.read():  # 1バイト読む, ボタンを押すとArduinoから1が送られる
+            print("serial catched")
 
 
 if __name__ == '__main__':
